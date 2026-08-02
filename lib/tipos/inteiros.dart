3e14bc5fff022b3fab82fbd62ec4b4619835baeb
Jonathas Barbosa <jotajonathas@gmail.com>
2026-08-02 17:51:14 -0300

    Aulas inteiros

---
+++ lib/tipos/inteiros.dart
@@ -0,0 +1,17 @@
+void main(){
+
+  int altura = 183;
+  print("Altura: $altura cm");
+
+  int ano = 2026;
+  print("Ano $ano");
+
+  int idade = 42;
+  print("Idade: $idade");
+
+  int dataNascimento = ano - idade;
+  print(dataNascimento);
+
+
+
+}
