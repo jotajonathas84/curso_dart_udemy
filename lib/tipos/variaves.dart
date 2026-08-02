4ec6e5b6c2d741a071ef400fd00f868d457c61b6
Jonathas Barbosa <jotajonathas@gmail.com>
2026-08-02 17:28:32 -0300

    Aula variávess

---
+++ lib/tipos/variaves.dart
@@ -0,0 +1,32 @@
+void main(){
+  var nome = "Jonathas";
+      nome = "Jonathas Barbosa";
+      print(nome);
+
+  var idade = 42;
+  print(idade);
+
+  var peso = 83.5;
+      peso = 80;
+  print(peso);
+
+  var maiotIdade = false;
+      maiotIdade = true;
+  print(maiotIdade);
+
+const nomeCompleto = "Jonathas Santos Barbosa";
+
+print(nomeCompleto);
+      
+final int altura = 184;
+print(altura);
+
+late String nomePai;
+
+nomePai = "Carlos";
+
+print(nomePai);
+
+
+
+}
