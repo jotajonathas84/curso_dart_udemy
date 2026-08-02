d0e7287c3c6817adeaec4f375c17a923599fbf74
Jonathas Barbosa <jotajonathas@gmail.com>
2026-08-02 16:49:05 -0300

    Iniciando projeto Dart

---
+++ bin/curso_dart_udemy.dart
@@ -0,0 +1,5 @@
+import 'package:curso_dart_udemy/curso_dart_udemy.dart' as curso_dart_udemy;
+
+void main(List<String> arguments) {
+  print('Hello world: ${curso_dart_udemy.calculate()}!');
+}
---
+++ test/curso_dart_udemy_test.dart
@@ -0,0 +1,8 @@
+import 'package:curso_dart_udemy/curso_dart_udemy.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
